b8566edcc2dacc4b958e4887b224f53aaea2036a
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-02 19:08:07 -0300

    added code

---
+++ main.py
@@ -0,0 +1,8 @@
+#Receiving height value from user
+height = float(input("Digite sua altura :\n"))
+
+#Calculating ideal weight
+iweight = (72.7 * height) - 58
+
+#Printing value for the user
+print(f"Seu peso ideal é {iweight}")
